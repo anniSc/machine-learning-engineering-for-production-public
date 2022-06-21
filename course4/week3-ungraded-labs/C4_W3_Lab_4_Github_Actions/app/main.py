0335b4ba841b52b5c41606f3268963eba33a248e
anniSc <45626818+anniSc@users.noreply.github.com>
2022-06-21 16:19:08 +0200

    Adding new classifier

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -4,23 +4,24 @@ from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
-
+# Hi
 
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
-with open("models/wine.pkl", "rb") as file:
+with open("models/wine-95.pkl", "rb") as file:
     clf = pickle.load(file)
 
 
 class Wine(BaseModel):
     batches: List[conlist(item_type=float, min_items=13, max_items=13)]
 
+# New test comment
 
 @app.post("/predict")
 def predict(wine: Wine):
     batches = wine.batches
     np_batches = np.array(batches)
     pred = clf.predict(np_batches).tolist()
-    return {"Prediction": pred}
-    # Test comment
+    return {"New Prediction": pred}
+    # Test
